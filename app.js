b71398acaa099e9f76c14202fa2f661f225640e9
Islam <islam.mostafa.fahmey@gmail.com>
2021-08-05 19:39:34 +0200

    add pdf output feature

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const { createReport } = require("docx-templates");
 const xlsx = require("node-xlsx");
-
 const fs = require("fs");
+const { PDFNet } = require("@pdftron/pdfnet-node");
+
 const template = fs.readFileSync("input/template.docx");
 const workSheetsFromFile = xlsx.parse(`input/input.xlsx`);
 const keys = workSheetsFromFile[0].data[0];
-
 const data = [];
 const dataArray = workSheetsFromFile[0].data;
 for (let i = 1; i < dataArray.length; i++) {
@@ -18,6 +18,10 @@ for (let i = 1; i < dataArray.length; i++) {
 }
 
 data.forEach(async (patient, i) => {
+  patient.patientName = patient.patientName.toUpperCase();
+  patient.result = patient.result.toUpperCase();
+
+  await PDFNet.initialize();
   const buffer = await createReport({
     template,
     data: patient,
@@ -26,4 +30,22 @@ data.forEach(async (patient, i) => {
     `output/${patient.patientName}-${patient.accessionNumber}.docx`,
     buffer
   );
+  // toPdf(
+  //   `output/${patient.patientName}-${patient.accessionNumber}.docx`,
+  //   `output/${patient.patientName}-${patient.accessionNumber}.pdf`
+  // );
+
+  // perform the conversion with no optional parameters
+  const pdfdoc = await PDFNet.Convert.officeToPdfWithPath(
+    `output/${patient.patientName}-${patient.accessionNumber}.docx`
+  );
+
+  // save the result
+  await pdfdoc.save(
+    `output/${patient.patientName}-${patient.accessionNumber}.pdf`,
+    PDFNet.SDFDoc.SaveOptions.e_linearized
+  );
+  if (Object.is(data.length - 1, i)) {
+    process.exit();
+  }
 });
